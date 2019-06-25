dfd038914520e7cefc724797ff27a57ca97bb6c7
Diego Gaspar <diego.gaspar@alu.ufc.br>
2019-06-25 14:06:06 -0300

    Trabalho final web

---
+++ src/main/java/com/deliveryapp/DeliveryappApplication.java
@@ -0,0 +1,14 @@
+package com.deliveryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryappApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+        SpringApplication.run(DeliveryappApplication.class, args);
+    }
+
+}
